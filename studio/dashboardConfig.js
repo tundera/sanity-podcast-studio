382e6d7c8c32a6f6cf80f559a1a359d8572cd319
Sanity.io <hello@sanity.io>
2021-03-15 08:34:03 -0600

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,43 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials'
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '',
+                  title: 'Sanity Studio',
+                  name: 'sanity-podcast-studio-studio-vaoybtfs',
+                  apiId: '6eeb7524-f0f8-413d-b2c2-448af23995b8'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/tundera/sanity-podcast-studio',
+            category: 'Code'
+          }
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recently edited', order: '_updatedAt desc', limit: 10, types: []},
+      layout: {width: 'medium'}
+    }
+  ]
+}
